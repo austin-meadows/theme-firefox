1284a84684976c2771e7e82c80f39be2780863f6
Austin Meadows <austin-meadows@users.noreply.github.com>
2021-12-28 13:12:07 -0500

    rename folder, update build to copy to ff profile

--- README.md
+++ README.md
@@ -10,11 +10,14 @@ Based on the excellent [waterfall](https://github.com/crambaud/waterfall) Firefo
 
 - Makes the dropdown auto suggestions more apparent
 
+- Gives 5px border radius to some UI elements
+
 - `userChrome.css` is minified with [csso](https://github.com/css/csso)
 
 # Scripts
 
 ```sh
 # Builds `src/theme.css` to `dist/userChrome.css`
+# Copies `dist/userChrome.css` to `[FF_PROFILE_PATH]/chrome/userChrome.css`
 npm run build
 ```
--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,27 @@
 import { minify } from "csso";
 import fs from "fs";
+import ini from "ini";
 
+//// Config
 const inputFile = "src/theme.css";
-const outputFile = "dist/userChrome.css";
+const outputFile = "build/userChrome.css";
+// (may work for macOS)
+const ffPath = `${process.env.APPDATA}/Mozilla/Firefox`;
 
+//// CSS File Creation
 const input = fs.readFileSync(inputFile, "utf8");
 const output = minify(input, {
   filename: inputFile,
 });
 
-fs.writeFile(outputFile, output.css, () => {
-  console.log("File written to dist/userChrome.css");
-});
+fs.writeFileSync(outputFile, output.css);
+console.log(`File written to ${outputFile}`);
+
+//// Copies built file to Firefox profile
+// comment this out if you don't want this step
+const {
+  Profile0: { Path: ffProfilePath },
+} = ini.parse(fs.readFileSync(`${ffPath}/profiles.ini`, "utf-8"));
+
+fs.copyFileSync(outputFile, `${ffPath}/${ffProfilePath}/chrome/userChrome.css`);
+console.log(`File written to ${ffProfilePath}`);
